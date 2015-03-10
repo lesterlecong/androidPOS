7de546ab716a054149edad55272d14d13e281315
Lester <lester@hggphilippines.com>
2015-03-10 17:54:35 +0800

    testing how to get screen size

--- app/src/main/java/com/project/pointofsale/androidpos/MainActivity.java
+++ app/src/main/java/com/project/pointofsale/androidpos/MainActivity.java
@@ -1,9 +1,15 @@
 package com.project.pointofsale.androidpos;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +18,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getScreenResolution(this);
+        getScreenDPI();
     }
 
 
@@ -35,5 +43,26 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private static void getScreenResolution(Context context){
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+
+        Log.d("SCREEN_SIZES", "Width:"+width+", Height:"+height);
+    }
+
+    private void getScreenDPI(){
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        float xDpi = dm.xdpi;
+        float yDpi = dm.ydpi;
+
+        Log.d("SCREEN_SIZES", "x:"+xDpi+", y:"+yDpi);
     }
 }
